925ea8d44e96f3500c9a1ba3599dc9e0cfebb2b9
ali_vs <alig789@gmail.com>
2022-03-12 19:27:45 -0500

    merged

---
+++ submission.js
@@ -0,0 +1,9 @@
+const webcamElement = document.getElementById('webcam');
+const canvasElement = document.getElementById('canvas');
+const snapSoundElement = document.getElementById('snapSound');
+const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
+
+
+
+
+let picture = webcam.snap();
